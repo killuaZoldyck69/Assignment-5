a6bee634932f1278ce849ae1addd3eb93779fad9
killuaZolyck69 <tanver694225@gmail.com>
2024-09-27 21:49:58 +0600

    add donet feature

--- JS Files/common-function.js
+++ JS Files/common-function.js
@@ -12,3 +12,23 @@ function getValue(id) {
 function getInnerText(id) {
   return document.getElementById(id).innerText;
 }
+
+// donet function
+function donetFunction(id1, id2, id3) {
+  getEleID(id1).addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const currentBalance = parseFloat(getInnerText("current-balance"));
+    const donetAmount = parseFloat(getValue(id2));
+    const noakhaliBalance = parseFloat(getInnerText(id3));
+    if (donetAmount > 0 && currentBalance > 0) {
+      const newBalance = currentBalance - donetAmount;
+      document.getElementById("current-balance").innerText =
+        newBalance + " BDT";
+      const totalDonet = noakhaliBalance + donetAmount;
+      document.getElementById(id3).innerText = totalDonet + " BDT";
+    } else {
+      alert("You can not donet");
+    }
+  });
+}
---
+++ JS Files/donet.js
@@ -0,0 +1,3 @@
+donetFunction("noakhali-donet", "noakhali-amount", "noakhali-balance");
+donetFunction("feni-donet", "feni-amount", "feni-balance");
+donetFunction("quota-donet", "quota-amount", "quota-balance");
